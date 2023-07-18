f3535e414d59c4950e5b5ed75293709c94b93cc5
jeffclaybrook <48254646+jeffclaybrook@users.noreply.github.com>
2023-07-18 17:26:13 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,35 +1,26 @@
-const counter = document.getElementById('counter');
-const button = document.getElementById('button');
+const counter = document.getElementById("counter");
+const button = document.getElementById("button");
 
 let count = JSON.parse(localStorage.getItem("count") || "0");
 counter.innerText = count;
 
-button.addEventListener('click', () => {
+button.addEventListener("click", () => {
  count++;
  localStorage.setItem("count", JSON.stringify(count));
  counter.innerText = count;
 })
 
-function createRipple(e) {
- const element = e.currentTarget;
- const circle = document.createElement('span');
- const diameter = Math.max(element.clientWidth, element.clientHeight);
- const radius = diameter / 2;
-
- circle.style.width = circle.style.height = `${diameter}px`;
- circle.style.left = `${e.clientX - element.offsetLeft - radius}px`;
- circle.style.top = `${e.clientY - element.offsetTop - radius}px`;
- circle.classList.add('ripple');
-
- const ripple = element.getElementsByClassName('ripple')[0];
-
- if (ripple) ripple.remove();
-
- element.appendChild(circle);
+const animateButton = (e) => {
+ e.preventDefault;
+ e.target.classList.remove("animated");
+ e.target.classList.add("animated");
+ setTimeout(() => {
+  e.target.classList.remove("animated")
+ }, 700);
 }
 
-const buttons = [button];
+const buttons = document.getElementsByClassName("button");
 
-for (const button of buttons) {
- button.addEventListener('click', createRipple)
+for (let i = 0; i < buttons.length; i++) {
+ buttons[i].addEventListener("click", animateButton);
 }
